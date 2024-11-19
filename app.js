ef47df0fb141f9f20a14d8622f27f5f2ba43efc2
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-11-19 18:09:16 +0100

    aggiungo le opreazioni di base

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const posts = require('./posts')
+
 const app = express();
 
 const port = 3000;
@@ -15,16 +17,38 @@ app.get('/', (req, res) => {
 // creo le crud su post
 
 //index
+app.get('/posts', (req, res) => {
+    res.json(posts);
+})
 
 //show
+app.get('/posts/:slug', (req, res) => {
+    const slug = req.params.slug;
+    const post = posts.find((post) => post.slug === slug);
+    res.json(post);
+});
 
 //store
+app.post('/posts', (req, res) => {
+    res.send('creo un nuovo post')
+})
+
 
 //update
+app.put('/posts/:slug', (req, res) => {
+    res.send('aggiorno un post')
+})
 
 //modify
+app.patch('/posts/:slug', (req, res) => {
+    res.send('modifico un post')
+})
 
 //destroy
+app.delete('/posts/:slug', (req, res) => {
+    res.send('elimino un post')
+})
+
 
 
 app.listen(port, () => {
